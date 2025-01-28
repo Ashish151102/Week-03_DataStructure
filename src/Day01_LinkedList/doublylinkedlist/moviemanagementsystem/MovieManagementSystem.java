5b6ed160be867a8434b957e5cced03decd462ee1
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-28 21:04:36 +0530

    Implemented Movie Management System using a Doubly Linked List with functionalities to add, remove, search, update availability, display movie record in forward and reverse order.

---
+++ src/Day01_LinkedList/doublylinkedlist/moviemanagementsystem/MovieManagementSystem.java
@@ -0,0 +1,32 @@
+package Day01_LinkedList.doublylinkedlist.moviemanagementsystem;
+
+public class MovieManagementSystem {
+    public static void main(String[] args) {
+        MovieLinkedList movieList = new MovieLinkedList();
+
+        movieList.addAtBeginning("Inception", "Christopher Nolan", 2010, 4.3);
+        movieList.addAtBeginning("Inception2", "Christopher Nolan", 2010, 4.7);
+        movieList.addAtBeginning("Inception3", "Christopher Nolan", 2010, 4.3);
+        movieList.addAtBeginning("Inception4", "Christopher Nolan", 2010, 4.3);
+
+        movieList.addAtPosition("Inception5", "Christopher Nolan", 2010, 4.3, 3);
+        movieList.addAtPosition("Inception6", "Christopher Nolan", 2010, 4.3, 4);
+        movieList.addAtPosition("Inception7", "Christopher Nolan", 2010, 4.3, 2);
+
+        movieList.addAtEnd("Inception10", "Christopher Nolan", 2010, 4.3);
+        movieList.addAtEnd("Inception100", "Christopher Nolan", 2010, 4.3);
+//        movieList.displayMovieList();
+
+        MovieNode searchNode = movieList.searchMovie("Inception4", 4.7);
+        if(searchNode != null){
+            System.out.println("Movie Title: " + searchNode.movieTitle);
+            System.out.println();
+        }
+
+        System.out.println("\n\n");
+        movieList.displayMovieListReverse();
+
+
+    }
+}
+
---
+++ src/Day01_LinkedList/doublylinkedlist/moviemanagementsystem/MovieNode.java
@@ -0,0 +1,20 @@
+package Day01_LinkedList.doublylinkedlist.moviemanagementsystem;
+
+public class MovieNode {
+    String movieTitle;
+    String director;
+    int yearOfRelease;
+    double rating;
+    MovieNode next;
+    MovieNode prev;
+
+    public MovieNode(String movieTitle, String director, int yearOfRelease, double rating) {
+        this.director = director;
+        this.movieTitle = movieTitle;
+        this.yearOfRelease = yearOfRelease;
+        this.rating = rating;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
