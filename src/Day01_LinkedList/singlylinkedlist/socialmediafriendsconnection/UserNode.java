96d6460119677acaf78dbc27dbb5f18b3ec5da79
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-28 20:26:22 +0530

    Implemented Social Media Friend Connections using Singly Linked List with functionalities for adding/removing connections, finding mutual friends, displaying friends, searching users, and counting friend connections.

---
+++ src/Day01_LinkedList/singlylinkedlist/socialmediafriendsconnection/UserNode.java
@@ -0,0 +1,21 @@
+package Day01_LinkedList.singlylinkedlist.socialmediafriendsconnection;
+
+import java.util.ArrayList;
+
+    public class UserNode {
+        int userId;
+        String name;
+        int age;
+        ArrayList<Integer> friendIds; // List of friend IDs
+        UserNode next;
+
+        public UserNode(int userId, String name, int age) {
+            this.userId = userId;
+            this.name = name;
+            this.age = age;
+            this.friendIds = new ArrayList<>();
+            this.next = null;
+        }
+    }
+
+
