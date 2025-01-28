20bd6f5823601a34162bb51f730f2050fbe06de1
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-28 20:42:36 +0530

    Implemented Student Record Management using Singly Linked List with functionalities to add, delete, search, display, and update student records by Roll Number.

---
+++ src/Day01_LinkedList/singlylinkedlist/studentrecordmanagement/StudentNode.java
@@ -0,0 +1,19 @@
+package Day01_LinkedList.singlylinkedlist.studentrecordmanagement;
+
+class StudentNode {
+    int rollNumber;
+    String name;
+    int age;
+    char grade;
+    StudentNode next;
+
+    // Constructor
+    public StudentNode(int rollNumber, String name, int age, char grade) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}
+
