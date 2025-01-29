8c6d73de16013cb908f6e1364e42b6966e4a8b2b
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-29 14:59:53 +0530

    Implemented Online ticket reservation system using a Circular Linked List with functionalities to add new ticket at various positions, remove ticket by Ticket ID, view and navigate tasks circularly, display all current tickets, and search Search for a ticket by Customer Name or Movie Name.

---
+++ src/Day01_LinkedList/circularlinkedlist/ticketreservationsystem/TicketNode.java
@@ -0,0 +1,21 @@
+package Day01_LinkedList.circularlinkedlist.ticketreservationsystem;
+
+    public class TicketNode {
+        int ticketID;
+        String customerName;
+        String movieName;
+        String seatNumber;
+        String bookingTime;
+        TicketNode next;
+
+        public TicketNode(int ticketID, String customerName, String movieName, String seatNumber, String bookingTime) {
+            this.ticketID = ticketID;
+            this.customerName = customerName;
+            this.movieName = movieName;
+            this.seatNumber = seatNumber;
+            this.bookingTime = bookingTime;
+            this.next = null; // Initialize next as null
+        }
+    }
+
+
