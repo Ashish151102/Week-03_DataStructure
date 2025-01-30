0f773d5575212f26c99b666d27f5f1abd0d04e0c
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 21:40:16 +0530

    Implemented quick sort to sort product price in ascending order.

---
+++ src/Day03_sortingAlgoritm/quicksort/sortproductprice/Main.java
@@ -0,0 +1,16 @@
+package Day03_sortingAlgoritm.quicksort.sortproductprice;
+public class Main {
+    public static void main(String[] args) {
+        double[] productPrices = {6, 3, 1, 8, 7, 10, 13, 11};
+
+        QuickSort obj = new QuickSort();
+        double[] sortedPriceList = obj.quickSort(productPrices, 0, productPrices.length-1);
+
+        for(double price : sortedPriceList){
+            System.out.print(price + "  ");
+        }
+
+    }
+}
+
+
