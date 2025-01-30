455c5fafdb12cef2909e3901e10fb382d6f68f95
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 20:07:16 +0530

    Implemented Two Sum problem solution using a hash map for efficient index lookup.

---
+++ src/Day02_StackQueue_HashMap/hashmapandhashfunctions/twosum/Main.java
@@ -0,0 +1,19 @@
+package Day02_StackQueue_HashMap.hashmapandhashfunctions.twosum;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 6, 3, 100, 45, 32, 15, 6, 3};
+        int findSum = 9;
+
+        FindPairIndices obj = new FindPairIndices();
+        List<int[]> indicesList = obj.findPairIndices(arr, findSum);
+
+        // displaying all pair of indices
+        for(int[] subarray : indicesList){
+            System.out.println(subarray[0] + "  " + subarray[1]);
+        }
+    }
+}
+
