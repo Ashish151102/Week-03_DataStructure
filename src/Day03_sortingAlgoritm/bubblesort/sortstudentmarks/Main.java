77823845bd699403988b9d1def8f4b32ca76881f
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 21:17:45 +0530

    Implemented Bubble Sort to sort student marks in ascending order.

---
+++ src/Day03_sortingAlgoritm/bubblesort/sortstudentmarks/Main.java
@@ -0,0 +1,16 @@
+package Day03_sortingAlgoritm.bubblesort.sortstudentmarks;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] studentMarks = {2, 6, 3, 8, 5, 1, 8, 9, 13, 10, 11};
+
+        BubbleSort obj = new BubbleSort();
+
+        int[] sortedMarksArray = obj.bubbleSort(studentMarks);
+
+        for(int element : sortedMarksArray){
+            System.out.print(element + "  ");
+        }
+    }
+}
+
