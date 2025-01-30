12ae2d10270100c1c05394cb2672599e96ee4172
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 10:54:16 +0530

    "Implemented Circular Tour Problem using a queue to determine the starting petrol pump for completing a circular tour based on surplus petrol tracking.

---
+++ src/Day02_StackQueue_HashMap/stackandqueueproblem/circulartour/Pump.java
@@ -0,0 +1,11 @@
+package Day02_StackQueue_HashMap.stackandqueueproblem.circulartour;
+
+public class Pump {
+    public double distance;
+    public double petrol;
+
+    public Pump(double distance, double petrol){
+        this.distance = distance;
+        this.petrol = petrol;
+    }
+}
