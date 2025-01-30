0279bb50cc9c76c56fe5a34eb2142a5d41828894
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 19:31:42 +0530

    Implemented algorithm to check for a pair with a given sum using a hash map for efficient lookups.

---
+++ src/Day02_StackQueue_HashMap/hashmapandhashfunctions/pairwithgivensum/PairExists.java
@@ -0,0 +1,18 @@
+package Day02_StackQueue_HashMap.hashmapandhashfunctions.pairwithgivensum;
+
+import java.util.HashMap;
+
+public class PairExists {
+    public boolean hasPair(int[] arr, int target){
+        HashMap<Integer, Boolean> elementsFound = new HashMap<>();
+
+        for (int j : arr) {
+            if (elementsFound.containsKey(target - j)) {
+                return true;
+            }
+            elementsFound.put(j, true);
+        }
+        return false;
+    }
+}
+
