fe6f4d8037a52a04d17d2c22c980450a3aacf9ee
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 20:03:20 +0530

    Implemented function to find the longest consecutive sequence in an unsorted array using a hash map.

---
+++ src/Day02_StackQueue_HashMap/hashmapandhashfunctions/longestconsecutivesequence/Main.java
@@ -0,0 +1,13 @@
+package Day02_StackQueue_HashMap.hashmapandhashfunctions.longestconsecutivesequence;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {100, 1, 200, 3, 5, 2, 4, 101};
+
+        FindLongestConsecutiveSequence obj = new FindLongestConsecutiveSequence();
+        int longest = obj.longestConsecutiveSequence(arr);
+
+        System.out.println("Length of longest consecutive sequence : " + longest);
+    }
+}
+
