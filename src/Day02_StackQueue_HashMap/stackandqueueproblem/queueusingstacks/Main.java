35e929fce5f142cf2d2b3869d476bfa821a8f45a
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 11:00:13 +0530

    Implemented a Queue Using Stacks, enqueue and dequeue operations are performed efficiently.

---
+++ src/Day02_StackQueue_HashMap/stackandqueueproblem/queueusingstacks/Main.java
@@ -0,0 +1,18 @@
+package Day02_StackQueue_HashMap.stackandqueueproblem.queueusingstacks;
+
+
+public class Main {
+    public static void main(String[] args) {
+        QueueUsingStacks queue = new QueueUsingStacks();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+
+        queue.display();
+        System.out.println("Removing element : " + queue.dequeue());
+        queue.display();
+    }
+}
