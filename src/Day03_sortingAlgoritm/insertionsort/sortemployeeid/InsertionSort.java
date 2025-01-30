3e68ddd452ecadea3b8b6239739c6dc45fa56490
Ashish151102 <ashishkumyadav789@gmail.com>
2025-01-30 21:21:51 +0530

    Implemented Insertion sort to sort employees id in ascending order.

---
+++ src/Day03_sortingAlgoritm/insertionsort/sortemployeeid/InsertionSort.java
@@ -0,0 +1,23 @@
+package Day03_sortingAlgoritm.insertionsort.sortemployeeid;
+
+public class InsertionSort {
+    public int[] insertionSort(int[] employeeIds){
+
+        int arrayLength = employeeIds.length;
+
+        for (int i = 0; i < arrayLength ; i++) {
+            int currentId = employeeIds[i];
+            int j = i - 1;
+
+            while(j >= 0 && employeeIds[j] > currentId){
+                employeeIds[j+1] = employeeIds[j];
+                j--;
+            }
+
+            employeeIds[j + 1] = currentId;
+        }
+
+        return employeeIds;
+    }
+}
+
---
+++ src/Day03_sortingAlgoritm/insertionsort/sortemployeeid/Main.java
@@ -0,0 +1,14 @@
+package Day03_sortingAlgoritm.insertionsort.sortemployeeid;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] employeeIds = {105, 103, 108, 110, 116, 113, 111, 119};
+
+        InsertionSort obj = new InsertionSort();
+        int[] sortedEmployeeIds = obj.insertionSort(employeeIds);
+
+        for(int id : employeeIds){
+            System.out.print(id + "  ");
+        }
+    }
+}
