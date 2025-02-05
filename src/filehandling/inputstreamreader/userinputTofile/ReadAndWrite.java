7520e109c1f6baa6a77cf48e8071cef8a6546362
Ashish151102 <ashishkumyadav789@gmail.com>
2025-02-05 16:10:42 +0530

    [Ashish] Add . Program to Read User Input and Write to File Using InputStreamReader.

---
+++ src/filehandling/inputstreamreader/userinputTofile/ReadAndWrite.java
@@ -0,0 +1,71 @@
+package filehandling.inputstreamreader.userinputTofile;
+
+import java.io.*;
+
+public class ReadAndWrite {
+    public static void consoleToFile(){
+        // creating InputStreamReader object
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+
+        // wrapping inputStreamReader into bufferedReader for efficient reading
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        // creating fileWriter Object
+        FileWriter fileWriter = null;
+
+        // bufferedWriter to write data efficiently
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            fileWriter = new FileWriter("C:\\Users\\ashish kumar\\OneDrive\\Desktop\\java.text");
+            bufferedWriter = new BufferedWriter(fileWriter);
+
+            if(fileWriter == null){
+                System.out.println("Nothing opened");
+                return;
+            }
+
+            // to read text from console
+            String line;
+
+            System.out.println("Enter text to be added to file (Type 'exit' to stop) : ");
+
+            while(!(line = bufferedReader.readLine()).equalsIgnoreCase("exit")){
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+
+            // to ensure data is written immediately
+            bufferedWriter.flush();
+
+            System.out.println("Data saved into the file !!");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try{
+                if(inputStreamReader != null){
+                    inputStreamReader.close();
+                }
+
+                if(bufferedReader != null){
+                    bufferedReader.close();
+                }
+                if(fileWriter != null){
+                    fileWriter.close();
+                }
+                if(bufferedWriter != null){
+                    bufferedWriter.close();
+                }
+            } catch (IOException e){
+                System.out.println("Error while closing resources : " + e.getMessage());
+            }
+        }
+    }
+
+    // main method
+    public static void main(String[] args) {
+        // calling method to read data from console and write it into a file
+        consoleToFile();
+    }
+}
